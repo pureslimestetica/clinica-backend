c342fa05904eacbe889acf5dc2dbc504c69361d7
pureslimestetica <pureslimestetica@gmail.com>
2025-10-02 14:53:15 -0300

    Update supabaseAdmin.ts

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseAdmin = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+const supabaseAdmin = createClient(
+  process.env.SUPABASE_URL as string,
+  process.env.SUPABASE_SERVICE_ROLE_KEY as string
 );
+
+// Exporta das duas formas (default e nomeado)
+export { supabaseAdmin };
+export default supabaseAdmin;
